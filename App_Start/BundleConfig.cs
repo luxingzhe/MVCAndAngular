7b5f096cedcde5685770d2d9bad8adb3aebf8112
Liu Su <liusu@microsoft.com>
2020-08-01 13:48:47 -0400

    git initiation

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace MVCAndAngular.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Script/Bundles").Include(
+                       "~/Bundles/runtime.*",
+                       "~/Bundles/zone.*",
+                       "~/Bundles/polyfills.*",
+                       "~/Bundles/main.*"));
+            bundles.Add(new StyleBundle("~/Content/Styles").Include("~/Bundles/styles.*"));
+        }
+    }
+}
